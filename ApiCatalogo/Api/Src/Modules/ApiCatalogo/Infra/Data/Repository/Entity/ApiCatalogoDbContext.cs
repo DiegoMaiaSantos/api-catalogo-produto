57e4f3becd56ca40f2f21090c1ad9579ef192b6b
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-09-23 13:26:31 -0300

    Criando o arquivo de contexto do Entity Framework Core

---
+++ ApiCatalogo/Api/Src/Modules/ApiCatalogo/Infra/Data/Repository/Entity/ApiCatalogoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Api.Src.Modules.ApiCatalogo.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Src.Modules.ApiCatalogo.Infra.Data.Repository.Entity
+{
+    public class ApiCatalogoDbContext : DbContext
+    {
+        public ApiCatalogoDbContext(DbContextOptions<ApiCatalogoDbContext> options) : base(options) 
+        {}
+
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+    }
+}
