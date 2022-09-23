c08dd7e39f1c7caafc6e3621659e46d7a8bbfc4a
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-09-22 21:00:49 -0300

    Criando as entidades do modelo de domínio

---
+++ ApiCatalogo/Api/Src/Modules/ApiCatalogo/Domain/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Src.Modules.ApiCatalogo.Domain.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? ImagemUrl { get; set; }
+    }
+}
---
+++ ApiCatalogo/Api/Src/Modules/ApiCatalogo/Domain/Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Src.Modules.ApiCatalogo.Domain.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string? ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
