905c78c4e45216a48a4c80c4b698fcddce05057c
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-09-26 21:36:22 -0300

    Definido o relacionamento entre as entidades de domínio

--- ApiCatalogo/Api/Src/Modules/ApiCatalogo/Domain/Models/Categoria.cs
+++ ApiCatalogo/Api/Src/Modules/ApiCatalogo/Domain/Models/Categoria.cs
@@ -1,9 +1,12 @@
-﻿namespace Api.Src.Modules.ApiCatalogo.Domain.Models
+﻿using System.Collections.ObjectModel;
+
+namespace Api.Src.Modules.ApiCatalogo.Domain.Models
 {
     public class Categoria
     {
         public int CategoriaId { get; set; }
         public string? Nome { get; set; }
         public string? ImagemUrl { get; set; }
+        public ICollection<Produto>? Produtos { get; set; } = new Collection<Produto>();
     }
 }
