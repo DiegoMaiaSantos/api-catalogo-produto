0ac50f1095eea7b46f64ca1774469ce605164e04
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-09-30 18:13:22 -0300

    (chore): Melhorias nas configuracoes de conexao da API

---
+++ ApiCatalogo/Api/Src/Config/Environments/Env.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Src.Config.Environments
+{
+    public static class Env
+    {
+        public static readonly Func<string, string> Get = (
+            env) => Environment.GetEnvironmentVariable(env);
+    }
+}
