04f2ca38721f6df7c574d164d5495d0f269d199a
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-10-01 21:22:10 -0300

    (chore): Implementado configuracoes para estabelecer uma conexao mais segura

--- ApiCatalogo/Api/Program.cs
+++ ApiCatalogo/Api/Program.cs
@@ -1,32 +1,6 @@
-using Api.Src.Modules.ApiCatalogo.Infra.Data.Repository.Entity;
-using Microsoft.EntityFrameworkCore;
+﻿using Api;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApiCatalogoDbContext>(options =>
-                    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+await Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+    .Build()
+    .RunAsync();
---
+++ ApiCatalogo/Api/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Api.Src.Shared.Infra.Ioc;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            services.AddCors(options =>
+                options.AddPolicy(
+                    "AllowAll", p =>
+                    {
+                        p.AllowAnyOrigin();
+                        p.AllowAnyMethod();
+                        p.AllowAnyHeader();
+                    }));
+
+            services.RegisterConnectionServices();
+            services.RegisterDependencies();
+            services.RegisterHealthChecks();
+            services.AddSwaggerConfig("ApiCatalogo",
+                                        "Api para controlar os produtos e suas categorias.");
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiCatalogo v1"));
+
+            }
+            app.UseCors("AllowAll");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
