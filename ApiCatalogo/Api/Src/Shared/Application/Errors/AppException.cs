723257bf95957891c71cf5c665711353ae92d5a5
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-10-01 22:40:37 -0300

    (chore): Criado classes para Tramento de erros e respotas padroes

---
+++ ApiCatalogo/Api/Src/Shared/Application/Errors/AppException.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace Api.Src.Shared.Application.Errors
+{
+    [Serializable]
+    public class AppException : Exception
+    {
+        public int? StatusCode { get; set; }
+
+        public AppException(string message)
+            : base(message)
+        {
+        }
+
+        public AppException(string message, int statusCode)
+            : base(message)
+        {
+            StatusCode = statusCode;
+        }
+
+        protected AppException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
---
+++ ApiCatalogo/Api/Src/Shared/Application/Http/Response/DefaultResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Src.Shared.Application.Http.Response
+{
+    public class DefaultResponse
+    {
+        public bool Sucess { get; set; }
+        public object? Data { get; set; }
+    }
+}
