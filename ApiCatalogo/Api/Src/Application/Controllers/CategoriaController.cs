9c504cb7d1b48556406d48c577331f89d10b4f1d
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-10-08 10:38:20 -0300

    (chore): Criado a interface de servico e repositorio e classe de servico

--- ApiCatalogo/Api/Src/Application/Controllers/CategoriaController.cs
+++ ApiCatalogo/Api/Src/Application/Controllers/CategoriaController.cs
@@ -1,4 +1,5 @@
-﻿using Api.Src.Shared.Application.Errors;
+﻿using Api.Src.Domain.Interfaces.Services;
+using Api.Src.Shared.Application.Errors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,26 +10,26 @@ namespace Api.Src.Application.Controllers
     public class CategoriaController : ControllerBase
     {
         private readonly ICategoriaService _categoriaService;
-    }
 
-    public CategoriaController(ICategoriaService categoriaService)
-    {
-        _categoriaService = categoriaService;
-    }
-
-    [HttpGet("{categoriaId}")]
-    public async Task<ActionResult> GetCategoria([FromRoute] int categoriaId)
-    {
-        try
+        public CategoriaController(ICategoriaService categoriaService)
         {
-            var data = await _categoriaService.Execute(categoriaId);
-
-            return Ok(data);
+            _categoriaService = categoriaService;
         }
-        catch (AppException ex)
+
+        [HttpGet("{categoriaId}")]
+        public async Task<ActionResult> GetCategoria([FromRoute] int categoriaId)
         {
-            return StatusCode(ex.StatusCode ?? StatusCodes.Status500InternalServerError,
-                new { ex.Message });
+            try
+            {
+                var data = await _categoriaService.Execute(categoriaId);
+
+                return Ok(data);
+            }
+            catch (AppException ex)
+            {
+                return StatusCode(ex.StatusCode ?? StatusCodes.Status500InternalServerError,
+                    new { ex.Message });
+            }
         }
     }
 }
---
+++ ApiCatalogo/Api/Src/Domain/Interfaces/Services/ICategoriaService.cs
@@ -0,0 +1,9 @@
+﻿using Api.Src.Modules.ApiCatalogo.Domain.Models;
+
+namespace Api.Src.Domain.Interfaces.Services
+{
+    public interface ICategoriaService
+    {
+        public Task<Categoria> Execute(int categoriaId);
+    }
+}
