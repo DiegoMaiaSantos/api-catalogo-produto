a1890b97fb55015bc6edd29400334c2c46755dfa
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-10-08 10:15:22 -0300

    (chore): Criando crontroller da categoria e desenvolvendo os endpoints

---
+++ ApiCatalogo/Api/Src/Application/Controllers/CategoriaController.cs
@@ -0,0 +1,34 @@
+﻿using Api.Src.Shared.Application.Errors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Src.Application.Controllers
+{
+    [Route("api/[catalogo]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly ICategoriaService _categoriaService;
+    }
+
+    public CategoriaController(ICategoriaService categoriaService)
+    {
+        _categoriaService = categoriaService;
+    }
+
+    [HttpGet("{categoriaId}")]
+    public async Task<ActionResult> GetCategoria([FromRoute] int categoriaId)
+    {
+        try
+        {
+            var data = await _categoriaService.Execute(categoriaId);
+
+            return Ok(data);
+        }
+        catch (AppException ex)
+        {
+            return StatusCode(ex.StatusCode ?? StatusCodes.Status500InternalServerError,
+                new { ex.Message });
+        }
+    }
+}
