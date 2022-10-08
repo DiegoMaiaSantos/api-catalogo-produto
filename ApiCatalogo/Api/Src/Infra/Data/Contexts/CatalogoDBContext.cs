f16bbc88a2afef23a0ac2759e612661b524d7df1
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-10-07 22:49:27 -0300

    (chore): Configuracoes da classe de context para relacionar o mapeamento

--- ApiCatalogo/Api/Src/Infra/Data/Contexts/CatalogoDBContext.cs
+++ ApiCatalogo/Api/Src/Infra/Data/Contexts/CatalogoDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Api.Src.Modules.ApiCatalogo.Domain.Models;
+﻿using Api.Src.Infra.Data.Mappers;
+using Api.Src.Modules.ApiCatalogo.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Src.Infra.Data.Contexts
@@ -7,10 +8,22 @@ namespace Api.Src.Infra.Data.Contexts
     {
         public virtual DbSet<Categoria>? Categorias { get; set; }
         public virtual DbSet<Produto>? Produtos { get; set; }
-
         public CatalogoDBContext(DbContextOptions<CatalogoDBContext> options) 
             : base(options)
         { 
+        }
+
+        private static void ApplyMappers(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoriaMapper());
+            modelBuilder.ApplyConfiguration(new ProdutoMapper());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ApplyMappers(modelBuilder);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
