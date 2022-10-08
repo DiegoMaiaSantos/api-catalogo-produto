94233a7e0412a85ae2fb76b4f82c15444ef24bb9
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-10-08 15:27:48 -0300

    (chore): Criado service da categoria

--- ApiCatalogo/Api/Src/Services/CategoriaService.cs
+++ ApiCatalogo/Api/Src/Services/CategoriaService.cs
@@ -1,4 +1,5 @@
-﻿using Api.Src.Domain.Interfaces.Services;
+﻿using Api.Src.Domain.Interfaces.Repositories;
+using Api.Src.Domain.Interfaces.Services;
 using Api.Src.Modules.ApiCatalogo.Domain.Models;
 using Api.Src.Shared.Application.Errors;
 using Serilog;
@@ -21,13 +22,15 @@ namespace Api.Src.Services
                 Categoria categoria = await _categoriaRepository.FindById(categoriaId);
 
                 if (categoria == null)
-                    throw new AppException("A categoria selecionada não existe.", 
+                    throw new AppException($"O id: {categoriaId} não existe na categoria.", 
                         StatusCodes.Status404NotFound);
+
+                return categoria;
             } 
             catch (Exception ex)
             {
                 Log.Error($"Erro ao executar o serviço que mostra as categorias: {ex.Message}");
-                throw new ArgumentException(ex.Message, ex.Message);
+                throw new ArgumentException(ex.Message);
             }
         }
     }
