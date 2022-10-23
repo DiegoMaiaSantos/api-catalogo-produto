857263eab58ff8964621ff9eee6df8d8da50ed87
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-10-23 19:32:00 -0300

    (chore): criado produtodto

---
+++ ApiCatalogo/Api/Src/Domain/Dtos/ProdutoDto.cs
@@ -0,0 +1,17 @@
+﻿using Api.Src.Modules.ApiCatalogo.Domain;
+
+namespace Api.Src.Domain.Dtos
+{
+    public class ProdutoDto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal? Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public int? Estoque { get; set; }
+        public DateTime? DataCadastro { get; set; } = DateTime.Now;
+        public int? CategoriaProduto { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
